847479188b7dfb401b736bf49bba176a71492e34
JujiuYey <1041556532@qq.com>
2026-07-01 00:19:46 +0800

    refactor(learning): rename feynman directory to feynman-practice
    Restructure the feynman learning page directory by renaming it to feynman-practice for better consistency with the route path. Update the main page entry point from workbench.tsx to index.tsx to follow standard directory conventions.

--- web/src/routes/_layout/learn/feynman-practice/$goalId/$objectiveId.tsx
+++ web/src/routes/_layout/learn/feynman-practice/$goalId/$objectiveId.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { FeynmanWorkbenchPage } from "@/pages/learning/feynman/workbench";
+import { FeynmanWorkbenchPage } from "@/pages/learning/feynman-practice";
 
 export const Route = createFileRoute("/_layout/learn/feynman-practice/$goalId/$objectiveId")({
   component: FeynmanWorkbenchPage,
